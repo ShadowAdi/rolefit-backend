f823d2c830c47ea429a46caa493754561b1075c6
ShadowAdi <shadowshukla76@gmail.com>
2026-04-20 09:45:23 +0530

    adde publication

---
+++ app/models/Publication.py
@@ -0,0 +1,24 @@
+from app.db.db import Base
+from sqlalchemy import Column, String, DateTime, ForeignKey, JSON
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import timezone, datetime
+import uuid
+
+
+class Publication(Base):
+    __tablename__ = "Publication"
+
+    id = Column(UUID, primary_key=True, default=uuid.uuid4)
+    title = Column(String, nullable=False, index=True)
+    publisher = Column(String, nullable=False)
+    publication_date = Column(DateTime, nullable=False)
+    authors = Column(JSON, nullable=False)
+    description = Column(String, nullable=True)
+    url = Column(String, nullable=True)
+    userId = Column(UUID, ForeignKey("User.id"), nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
