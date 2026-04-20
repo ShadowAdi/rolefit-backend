0f57dc98b7d3322edeaafb54e73a7f86e9f10002
ShadowAdi <shadowshukla76@gmail.com>
2026-04-20 09:51:33 +0530

    added experiuence schema

---
+++ app/models/Experience.py
@@ -0,0 +1,31 @@
+from app.db.db import Base
+from sqlalchemy import Column, String, DateTime, ForeignKey, JSON, Integer
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import timezone, datetime
+import uuid
+
+
+class Experience(Base):
+    __tablename__ = "Experience"
+
+    id = Column(UUID, primary_key=True, default=uuid.uuid4)
+    company_name = Column(String, nullable=False, index=True)
+    description = Column(String, nullable=False)
+    techStack = Column(JSON, nullable=False)
+    role = Column(String, nullable=False, index=True)
+    employment_type = Column(String, nullable=True)
+    location_type = Column(String, nullable=True)
+    location_details = Column(String, nullable=True)
+    start_date = Column(DateTime, nullable=True)
+    start_month = Column(Integer, nullable=True)
+    start_year = Column(Integer, nullable=True)
+    end_date = Column(DateTime, nullable=True)
+    end_month = Column(Integer, nullable=True)
+    end_year = Column(Integer, nullable=True)
+    userId = Column(UUID, ForeignKey("User.id"), nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
