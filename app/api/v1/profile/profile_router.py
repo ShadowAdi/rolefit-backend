5da25e3b9ca35e62d7800fa38b99075c912b4b7f
ShadowAdi <shadowshukla76@gmail.com>
2026-04-20 17:11:11 +0530

    added create profile

---
+++ app/api/v1/profile/profile_router.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Depends, status
+from app.schema.Profile import (
+    ProfileCreateRequest,
+    ProfileUpdateRequest,
+    ProfileResponse,
+)
+from sqlalchemy.orm import Session
+from app.db.db import get_db
+from .profile_service import ProfileServiceClass
+from datetime import datetime, timezone
+from app.models.User import User
+from app.dependency.dependencies import get_current_user
+
+router = APIRouter(prefix="/api/v1/profile", tags=["Profile"])
+
+
+@router.post(
+    "/",
+    response_model=ProfileResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_profile(data: ProfileCreateRequest, db: Session = Depends(get_db)):
+    """
+    Register a new user account.
+
+    Args:
+        data: User registration data (email, password, username, profile_url, bio)
+        db: Database session
+
+    Returns:
+        RegisterApiResponse: User account created with success status
+    """
+    profile = ProfileServiceClass.create_profile(db=db, data=data)
+
+    return ProfileResponse(
+        success=True,
+        status_code=status.HTTP_201_CREATED,
+        message="Profile registered successfully",
+        data=ProfileResponse.model_validate(profile),
+        timestamp=datetime.now(timezone.utc),
+    )
