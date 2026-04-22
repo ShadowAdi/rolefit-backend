36cc6b2eab3661ad150bf8ae269ccc2d08cf8f2d
ShadowAdi <shadowshukla76@gmail.com>
2026-04-22 16:41:13 +0530

    imporove more fields

--- app/models/JobDescription.py
+++ app/models/JobDescription.py
@@ -1,23 +1,56 @@
 from app.db.db import Base
-from sqlalchemy import Column, String, DateTime, ForeignKey, JSON, Integer
+from sqlalchemy import (
+    Column,
+    String,
+    DateTime,
+    Enum as SQLEnum,
+)
 from sqlalchemy.dialects.postgresql import UUID, ARRAY
 from datetime import timezone, datetime
 import uuid
+import enum
+
+
+class RoleTypeEnum(str, enum.Enum):
+    FULL_TIME = "Full-time"
+    INTERNSHIP = "Internship"
+    CONTRACT = "Contract"
+
+
+class LocationTypeEnum(str, enum.Enum):
+    REMOTE = "Remote"
+    HYBRID = "Hybrid"
+    ON_SITE = "On-site"
 
 
 class JobDescription(Base):
     __tablename__ = "JobDescription"
 
     id = Column(UUID, primary_key=True, default=uuid.uuid4)
-    Company = Column(String, nullable=False, index=True)
-    Role_Type = Column(String, nullable=False, default="Full-time", index=True)
-    Location = Column(String, nullable=False, index=True, default="Remote")
-    Location_City = Column(String, nullable=False)
-    Salary_Min = Column(String, nullable=False)
-    Salary_Max = Column(String, nullable=False)
-    Salary_Currency = Column(String, nullable=False)
+    Role_Name = Column(String, nullable=True, index=True)
+    Company = Column(String, nullable=True, index=True)
+    Role_Type = Column(
+        SQLEnum(RoleTypeEnum), nullable=True, default=RoleTypeEnum.FULL_TIME, index=True
+    )
+    Location = Column(
+        SQLEnum(LocationTypeEnum),
+        nullable=True,
+        default=LocationTypeEnum.REMOTE,
+        index=True,
+    )
+    Location_City = Column(String, nullable=True)
+    Salary_Min = Column(String, nullable=True)
+    Salary_Max = Column(String, nullable=True)
+    Salary_Currency = Column(String, nullable=True)
+    Duration = Column(String, nullable=True)
     Tech_Stack = Column(ARRAY(String), default=[])
     Required_Skills = Column(ARRAY(String), default=[])
-    Experience_Required = Column(ARRAY(String))
-    Summary = Column(String)
-    Raw_JD = Column(String)
+    Experience_Required = Column(String, nullable=True)
+    Summary = Column(String, nullable=True)
+    Raw_JD = Column(String, nullable=False)
+    Created_At = Column(DateTime, default=datetime.now(timezone.utc))
+    Updated_At = Column(
+        DateTime,
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc),
+    )
