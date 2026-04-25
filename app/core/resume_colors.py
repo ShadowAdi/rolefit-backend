32341722b234f3a28662e5ce9ded3f9f4388d4dc
ShadowAdi <shadowshukla76@gmail.com>
2026-04-25 11:59:12 +0530

    added resume colors

---
+++ app/core/resume_colors.py
@@ -0,0 +1,7 @@
+from reportlab.lib import colors
+
+ACCENT = colors.HexColor("#1a1a2e")
+SUBTEXT = colors.HexColor("#4a4a6a")
+RULE = colors.HexColor("#c8c8d8")
+BLACK = colors.black
+WHITE = colors.white
