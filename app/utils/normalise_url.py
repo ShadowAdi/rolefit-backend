67d7e8ac6cad00bbd807b10f1140963b2b634cc1
ShadowAdi <shadowshukla76@gmail.com>
2026-04-27 18:37:42 +0530

    normalise url

--- app/utils/normalise_url.py
+++ app/utils/normalise_url.py
@@ -1,2 +1,19 @@
+import re
+from urllib.parse import urlparse, parse_qs
+
+
 def _normalise_url(url: str) -> str:
-    pass
+    parsed = urlparse(url=url)
+
+    if "drive.google.com" in parsed.netloc:
+        m = re.search(r"/file/d/([^/]+)", parsed.path)
+        if m:
+            file_id = m.group(1)
+            return f"https://drive.google.com/uc?export=download&id={file_id}"
+
+        qs = parse_qs(parsed.query)
+        if "id" in qs:
+            file_id = qs["id"][0]
+            return f"https://drive.google.com/uc?export=download&id={file_id}"
+
+        return url
