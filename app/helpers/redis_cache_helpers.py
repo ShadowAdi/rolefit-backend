65f31d0824d0bcabc79febfcc20c5f51bf409442
ShadowAdi <shadowshukla76@gmail.com>
2026-04-30 19:02:54 +0530

    added get set helpers

---
+++ app/helpers/redis_cache_helpers.py
@@ -0,0 +1,16 @@
+from app.db.redis_db import redis
+from app.core.logger import logger
+
+
+async def get_cache(key: str):
+    if not redis:
+        logger.error("Redis not initialized. Call init_redis() first.")
+        raise RuntimeError("Redis not initialized. Call init_redis() first.")
+    return await redis.get(key)
+
+
+async def set_cache(key: str, value: str, ttl: int = 300):
+    if not redis:
+        logger.error("Redis not initialized. Call init_redis() first.")
+        raise RuntimeError("Redis not initialized. Call init_redis() first.")
+    await redis.set(key, value, ex=ttl)
