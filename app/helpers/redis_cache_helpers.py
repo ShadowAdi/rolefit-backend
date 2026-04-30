7d61431bc1dec8bc101959e885dd451def8fcce0
ShadowAdi <shadowshukla76@gmail.com>
2026-04-30 19:04:15 +0530

    added delete

--- app/helpers/redis_cache_helpers.py
+++ app/helpers/redis_cache_helpers.py
@@ -14,3 +14,10 @@ async def set_cache(key: str, value: str, ttl: int = 300):
         logger.error("Redis not initialized. Call init_redis() first.")
         raise RuntimeError("Redis not initialized. Call init_redis() first.")
     await redis.set(key, value, ex=ttl)
+
+
+async def delete_cache(key: str):
+    if not redis:
+        logger.error("Redis not initialized. Call init_redis() first.")
+        raise RuntimeError("Redis not initialized. Call init_redis() first.")
+    await redis.delete(key)
