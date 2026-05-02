f202982d52452d84671b19ddfd7c992de75ec91e
ShadowAdi <shadowshukla76@gmail.com>
2026-05-02 20:37:11 +0530

    quick fix

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -10,18 +10,9 @@ from .ai_tasks import (
     cleanup_old_tasks,
 )
 
-from .pdf_tasks import (
-    generate_resume_pdf,
-    generate_cover_letter_pdf,
-    generate_resume_and_cover_letter,
-)
-
 __all__ = [
     "generate_resume_with_ai",
     "generate_cover_letter_with_ai",
     "parse_job_description",
     "cleanup_old_tasks",
-    "generate_resume_pdf",
-    "generate_cover_letter_pdf",
-    "generate_resume_and_cover_letter",
 ]
