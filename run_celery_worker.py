cf2568f3e81e0a58043ea8f7f6fea7e4579c09e4
ShadowAdi <shadowshukla76@gmail.com>
2026-05-03 12:38:46 +0530

    fix run celeryworker

--- run_celery_worker.py
+++ run_celery_worker.py
@@ -1,6 +1,9 @@
 from app.core.celery_app import celery_app
 from app.core.logger import logger
 
+# Import tasks to register them with Celery
+import app.tasks  # This imports __init__.py which imports all tasks
+
 if __name__ == "__main__":
     logger.info("Starting Celery Worker...")
     celery_app.worker_main(
